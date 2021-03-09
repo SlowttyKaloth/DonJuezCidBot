852e714f5bfefd12c4905eaee354e96d2ff1a642
SlowttyKaloth <80235508+SlowttyKaloth@users.noreply.github.com>
2021-03-08 21:46:25 -0600

    Update __main__.py

--- botcid/__main__.py
+++ botcid/__main__.py
@@ -10,12 +10,14 @@ def start(update, context):
     update.message.reply_text('Bienvenido nuevo aventurero! veo que quieres iniciarte en este fantástico mundo de peleas eh? entendido!\n\nSi quiere iniciar su aventura por favor diga lo siguiente "/registrar" y acontinuacion seleciona tus datos para crear su licencia de aventurero y poder llevar registro de tus stats')
 
 def registrar(update, context):
-    update.message.reply_text('hola')
+
+    idsuser = 
+    update.message.reply_text(iduser)
 
 #funcion main VVVV--------------------------------------
 if __name__ == '__main__':
 
-    updater = Updater(token='1673134049:AAE-TvbIIn_Ii6gZPT4QgIBniiPV13UXZ-Y', use_context=True)
+    updater = Updater(token='token', use_context=True)
 
     dp = updater.dispatcher
 
